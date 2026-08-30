623ca57359bc2f3aa86b277f50edbce2b25d3369
GitHub git update Action <action@github.com>
2026-08-30 00:12:36 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_uaBrain.h
+++ generated/controllers/generated/signature_uaBrain.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3440338264
-#define TS_SIGNATURE "rusEFI main.2026.08.29.uaBrain.3440338264"
+#define SIGNATURE_HASH 1959033160
+#define TS_SIGNATURE "rusEFI main.2026.08.30.uaBrain.1959033160"
